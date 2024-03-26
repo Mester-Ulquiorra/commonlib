f83e82a614bf5e8b5e40ca3083f6ef44fc91f881
MesterMan03 <MesterMan03+github@proton.me>
2024-03-26 19:38:37 +0100

    1.1.5 - cleanup

--- build/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./lib/Log.js";
-export * from "./lib/database/index.js";
-export * from "./lib/types/index.js";
-export * from "./lib/Internal.js";
--- build/lib/database/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./GiveawayConfig.js";
-export * from "./LevelConfig.js";
-export * from "./PunishmentConfig.js";
-export * from "./TicketConfig.js";
-export * from "./TictactoeConfig.js";
-export * from "./UserConfig.js";
